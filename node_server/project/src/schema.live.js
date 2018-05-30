ffb45a28b39f3fc55e557c02058575ee9d0a8ea3
Arianne Dee <ariannedee@gmail.com>
2018-05-30 11:27:03 -0700

    Get users

--- node_server/project/src/schema.live.js
+++ node_server/project/src/schema.live.js
@@ -1,12 +1,28 @@
 var graphql = require('graphql');
+var knex = require('../db');
+
+const UserType = new graphql.GraphQLObjectType({
+    name: 'User',
+    description: 'A user object',
+    fields: {
+        id: {
+            type: graphql.GraphQLNonNull(graphql.GraphQLID),
+            resolve(user) {
+                return user.id;
+            }
+        }
+    }
+});
 
 var queryType = new graphql.GraphQLObjectType({
     name: 'Query',
     fields: {
-        hello: {
-            type: graphql.GraphQLString,
-            resolve() {
-                return "world";
+        users: {
+            type: new graphql.GraphQLList(UserType),
+            description: 'A list of users',
+            resolve(root) {
+                let query = knex('user');
+                return query;
             }
         }
     }
