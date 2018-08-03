615f437b7ccdda0897ed27d21f93f2f5810e0774
Arianne Dee <ariannedee@gmail.com>
2018-08-03 10:37:15 -0700

    Update index

---
+++ client/project/index-live.js
@@ -0,0 +1,55 @@
+const fullStar = "★";
+const emptyStar = "☆";
+
+const commitFragment = `
+fragment commitFragment on Repository {
+  ref(qualifiedName: "master") {
+    target {
+      ... on Commit {
+        history {
+          totalCount
+        }
+      }
+    }
+  }
+}
+`;
+
+let queryRepoList;
+
+let mutationAddStar;
+
+let mutationRemoveStar;
+
+function gqlRequest(query, variables, onSuccess) {
+  // MAKE GRAPHQL REQUEST
+  $.post({
+    url: "https://api.github.com/graphql",
+    headers: {
+      Authorization: "bearer ..."
+    },
+    contentType: "application/json",
+    data: JSON.stringify({
+      query: query,
+      variables: variables
+    }),
+    success: (response) => {
+      if (response.errors) {
+        console.log(response.errors);
+      } else {
+        onSuccess(response);
+      }
+    },
+    error: (error) => console.log(error)
+  })
+}
+
+function starHandler(element) {
+  // STAR OR UNSTAR REPO BASED ON ELEMENT STATE
+
+}
+
+$(window).ready(function() {
+  // GET NAME AND REPOSITORIES FOR VIEWER
+
+});
