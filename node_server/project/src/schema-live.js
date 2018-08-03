ffbc329c9b1501e84ddb28d53ff31349c76755c2
Arianne Dee <ariannedee@gmail.com>
2018-08-03 12:15:33 -0700

    Update with fake users

--- node_server/project/src/schema-live.js
+++ node_server/project/src/schema-live.js
@@ -1,12 +1,27 @@
 var graphql = require('graphql');
 
+const UserType = new graphql.GraphQLObjectType({
+  name: 'User',
+  description: 'This is a user',
+  fields: () => {
+    return {
+      id: {
+        type: graphql.GraphQLID,
+        resolve(user) {
+          return user.id;
+        }
+      }
+    }
+  }
+});
+
 var queryType = new graphql.GraphQLObjectType({
   name: 'Query',
   fields: {
-    hello: {
-      type: graphql.GraphQLString,
-      resolve () {
-        return 'world';
+    users: {
+      type: new graphql.GraphQLList(UserType),
+      resolve(root, args) {
+        return [{id: 1, username: 'admin'}];
       }
     }
   }
