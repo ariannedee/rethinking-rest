e20fbbb754bd747600b24c2c49136233ef052183
Arianne Dee <ariannedee@gmail.com>
2018-09-04 12:17:59 -0700

    Get users from db

--- node_server/project/src/schema-live.js
+++ node_server/project/src/schema-live.js
@@ -1,12 +1,26 @@
 const graphql = require('graphql');
+const knex = require('../db');
+
+const UserType = new graphql.GraphQLObjectType({
+  name: 'User',
+  description: 'This represents a user in the system',
+  fields: {
+    id: {
+      type: graphql.GraphQLID,
+      resolve(user) {
+        return user.id;
+      }
+    }
+  }
+});
 
 const queryType = new graphql.GraphQLObjectType({
   name: 'Query',
   fields: {
-    hello: {
-      type: graphql.GraphQLString,
-      resolve() {
-        return 'world';
+    users: {
+      type: new graphql.GraphQLList(UserType),
+      resolve(root) {
+        return knex('user');
       }
     }
   }
