0cf4bedd101779dce33138e54b2706582e21fee5
Arianne Dee <ariannedee@gmail.com>
2018-09-04 12:26:36 -0700

    Fill out user type

--- node_server/project/src/schema-live.js
+++ node_server/project/src/schema-live.js
@@ -10,7 +10,19 @@ const UserType = new graphql.GraphQLObjectType({
       resolve(user) {
         return user.id;
       }
-    }
+    },
+    username: {
+      type: graphql.GraphQLString,
+      resolve(user) {
+        return user.username;
+      }
+    },
+    isAdmin: {
+      type: graphql.GraphQLBoolean,
+      resolve(user) {
+        return user.role === 'admin';
+      }
+    },
   }
 });
 
