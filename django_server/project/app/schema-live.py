c80e7132b16d22b820b6780c862a4c40a81edb2a
Arianne Dee <ariannedee@gmail.com>
2018-09-04 12:46:52 -0700

    Add has read type

--- django_server/project/app/schema-live.py
+++ django_server/project/app/schema-live.py
@@ -3,7 +3,7 @@ import graphene_django
 
 from django.contrib.auth.backends import UserModel
 
-from .models import Book
+from .models import Book, HasRead
 
 
 class UserType(graphene_django.DjangoObjectType):
@@ -14,12 +14,18 @@ class UserType(graphene_django.DjangoObjectType):
 
     class Meta:
         model = UserModel
-        only_fields = ('id', 'username')
+        only_fields = ('id', 'username', 'books_read')
 
 
 class BookType(graphene_django.DjangoObjectType):
     class Meta:
         model = Book
+
+
+class HasReadType(graphene_django.DjangoObjectType):
+    class Meta:
+        model = HasRead
+
 
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
