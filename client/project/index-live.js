649887d10cbfb7db0fdd6235b984309b5a38ad86
Arianne Dee <ariannedee@gmail.com>
2018-10-01 08:32:39 -0700

    Reset live files

--- client/project/index-live.js
+++ client/project/index-live.js
@@ -15,26 +15,7 @@ fragment commitFragment on Repository {
 }
 `;
 
-const queryRepoList = `
-{
-  viewer {
-    name
-    repos: repositoriesContributedTo (first: 9, orderBy: {field: CREATED_AT, direction: DESC}) {
-      totalCount
-      nodes {
-        name
-        issues (states: OPEN) {
-          totalCount
-        }
-        prs: pullRequests (states: OPEN) {
-          totalCount
-        }
-        ... commitFragment
-      }
-    }
-  }
-}
-` + commitFragment;
+let queryRepoList;
 
 let mutationAddStar;
 
@@ -42,22 +23,6 @@ let mutationRemoveStar;
 
 function gqlRequest(query, variables, onSuccess) {
   // MAKE GRAPHQL REQUEST
-  $.post({
-    url: "https://api.github.com/graphql",
-    contentType: "application/json",
-    headers: {
-      Authorization: "bearer ..."
-    },
-    data: JSON.stringify({
-      query: query,
-      variables: variables
-    }),
-    success: (response) => {
-      console.log(response);
-      onSuccess(response)
-    },
-    error: (error) => console.log(error)
-  });
 
 }
 
@@ -68,22 +33,5 @@ function starHandler(element) {
 
 $(window).ready(function() {
   // GET NAME AND REPOSITORIES FOR VIEWER
-  gqlRequest(queryRepoList, {},
-    (response) => {
-      $('header h2').text(`Hello ${response.data.viewer.name}`);
-      const repos = response.data.viewer.repos;
-      console.log(repos);
-      if (repos.totalCount > 0) {
-        $('ul.repos').empty();
-      }
-      repos.nodes.forEach((repo) => {
-        const card = `
-          <h3>${repo.name}</h3>
-          <p>${repo.issues.totalCount} open issues</p>
-          <p>${repo.prs.totalCount} open pull requests</p>
-          <p>${repo.ref.target.history.totalCount} commits</p>
-        `
-        $('ul.repos').append(`<li><div>${card}</div></li>`)
-      });
-    });
+
 });
