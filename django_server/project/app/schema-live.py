c0e20e6e4d8bffc7587d47ee62a343fdbeec868e
Arianne Dee <ariannedee@gmail.com>
2018-10-01 12:59:00 -0700

    Add average rating

--- django_server/project/app/schema-live.py
+++ django_server/project/app/schema-live.py
@@ -1,23 +1,34 @@
 import graphene
 import graphene_django
 from django.contrib.auth.backends import UserModel
-from .models import Book
+from .models import Book, HasRead
+from django.db.models import Avg
 
 
 class UserType(graphene_django.DjangoObjectType):
     is_admin = graphene.Boolean()
+    average_rating = graphene.Float()
 
     def resolve_is_admin(self, info):
         return self.is_staff
 
+    def resolve_average_rating(self, info):
+        query = self.books_read.all().aggregate(Avg('rating'))
+        return query['rating__avg']
+
     class Meta:
         model = UserModel
-        only_fields = ('id', 'username')
+        only_fields = ('id', 'username', 'books_read')
 
 
 class BookType(graphene_django.DjangoObjectType):
     class Meta:
         model = Book
+
+
+class HasReadType(graphene_django.DjangoObjectType):
+    class Meta:
+        model = HasRead
 
 
 class Query(graphene.ObjectType):
