9424e4f746ed963ceff8a039e397d7f8fe44c86e
Arianne Dee <ariannedee@gmail.com>
2018-12-12 13:04:39 -0800

    Reset live files

--- client/project/index-live.js
+++ client/project/index-live.js
@@ -15,28 +15,7 @@ fragment commitFragment on Repository {
 }
 `;
 
-const queryRepoList = `
-{
-  viewer {
-    name
-    repos: repositoriesContributedTo (
-      first: 6, 
-      orderBy: {field: CREATED_AT, direction: DESC}
-    ) {
-      nodes {
-        name: nameWithOwner
-        issues (states: OPEN) {
-          totalCount
-        }
-        pullRequests (states: OPEN) {
-          totalCount
-        }
-        ... commitFragment
-      }
-    }
-  }
-}
-` + commitFragment;
+let queryRepoList;
 
 let mutationAddStar;
 
@@ -44,29 +23,7 @@ let mutationRemoveStar;
 
 function gqlRequest(query, variables, onSuccess) {
   // MAKE GRAPHQL REQUEST
-  $.post({
-    url: "https://api.github.com/graphql",
-    contentType: "application/json",
-    headers: {
-      Authorization: "bearer ..."
-    },
-    data: JSON.stringify({
-      query: query,
-      variables: variables
-    }),
-    success: (response) => {
-      console.log(response);
-      if (response.errors !== undefined) {
-        console.log("There were errors");
-        console.log(response.errors);
-      } else {
-        onSuccess(response.data);
-      }
-    },
-    error: (response) => {
-      console.log(response);
-    }
-  });
+
 }
 
 function starHandler(element) {
@@ -76,18 +33,5 @@ function starHandler(element) {
 
 $(window).ready(function() {
   // GET NAME AND REPOSITORIES FOR VIEWER
-  gqlRequest(queryRepoList, {}, (data) => {
-    $("header h2").text(`Hello ${data.viewer.name}`);
-    $("ul.repos").empty();
-    data.viewer.repos.nodes.forEach((repo) => {
-      const cardContent = `
-        <h3>${repo.name}</h3>
-        <p>${repo.issues.totalCount} open issues</p>
-        <p>${repo.pullRequests.totalCount} open PRs</p>
-        <p>${repo.ref.target.history.totalCount} commits</p>
-      `;
-      $("ul.repos").append(`<li><div>${cardContent}</div></li>`);
-    });
 
-  });
 });
