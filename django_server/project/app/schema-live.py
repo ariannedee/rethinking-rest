e8158324d5bc54ed4a155ef79868f7bce408a978
Arianne Dee <ariannedee@gmail.com>
2018-12-18 11:37:34 -0800

    Add books

--- django_server/project/app/schema-live.py
+++ django_server/project/app/schema-live.py
@@ -1,6 +1,7 @@
 import graphene
 import graphene_django
 from django.contrib.auth.backends import UserModel
+from .models import Book
 
 
 class UserType(graphene_django.DjangoObjectType):
@@ -8,15 +9,25 @@ class UserType(graphene_django.DjangoObjectType):
 
     def resolve_is_admin(self, info):
         return self.is_staff
-        
+
     class Meta(object):
         model = UserModel
         only_fields = ('id', 'username')
 
+
+class BookType(graphene_django.DjangoObjectType):
+    class Meta:
+        model = Book
+
+
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
+    books = graphene.List(BookType)
 
     def resolve_users(self, info):
         return UserModel.objects.all()
+
+    def resolve_books(self, info):
+        return Book.objects.all()
 
 schema = graphene.Schema(query=Query)
