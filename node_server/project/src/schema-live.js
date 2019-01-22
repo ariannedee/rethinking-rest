fba0d8b0bf6461f33621d7e17b38988f1f301d91
Arianne Dee <ariannedee@Ariannes-MacBook-Pro.local>
2019-01-22 10:55:06 -0800

    Hello world

---
+++ node_server/project/src/schema-live.js
@@ -0,0 +1,17 @@
+const graphql = require('graphql');
+
+const queryType = new graphql.GraphQLObjectType({
+    name: "Query",
+    fields: {
+        hello: {
+            type: graphql.GraphQLString,
+            resolve() {
+                return "world";
+            }
+        }
+    }
+});
+
+const schema = new graphql.GraphQLSchema({query: queryType});
+
+module.exports = schema;
