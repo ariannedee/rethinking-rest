152c70af357022b4e1e28cc0c910d02d70dbdf92
ariannedee <ariannedee@gmail.com>
2019-03-24 13:43:43 -0700

    Reset project files

--- client/project/index-live.js
+++ client/project/index-live.js
@@ -15,26 +15,7 @@ fragment commitFragment on Repository {
 }
 `;
 
-const queryRepoList = `
-{
-  viewer {
-    name
-    repositories(first: 6, orderBy: {field: CREATED_AT, direction: DESC}) {
-      totalCount
-      nodes {
-        name
-        openIssues: issues(states: OPEN) {
-          totalCount
-        }
-        openPRs: pullRequests(states: OPEN) {
-          totalCount
-        }
-        ... commitFragment
-      }
-    }
-  }
-}
-` + commitFragment;
+let queryRepoList;
 
 let mutationAddStar;
 
@@ -42,24 +23,7 @@ let mutationRemoveStar;
 
 function gqlRequest(query, variables, onSuccess) {
   // MAKE GRAPHQL REQUEST
-  $.post({
-    url: "https://api.github.com/graphql",
-    contentType: "application/json",
-    headers: {
-      Authorization: "bearer ..."
-    },
-    data: JSON.stringify({
-      query: query,
-      variables: variables
-    }),
-    success: (response) => {
-      if (response.errors) {
-        console.log(response.errors);
-      } else {
-        onSuccess(response);
-      }
-    }
-  });
+
 }
 
 function starHandler(element) {
@@ -69,21 +33,5 @@ function starHandler(element) {
 
 $(window).ready(function() {
   // GET NAME AND REPOSITORIES FOR VIEWER
-  gqlRequest(queryRepoList, {}, 
-    (response) => {
-      $("header h2").text(`Hello ${response.data.viewer.name}`);
-      const repos = response.data.viewer.repositories;
-      if (repos.totalCount > 0) {
-        $("ul.repos").empty();
-      }
-      repos.nodes.forEach((repo) => {
-        const card = `
-        <h3>${repo.name}</h3>
-        <p>${repo.openIssues.totalCount} open issues</p>
-        <p>${repo.openPRs.totalCount} open PRs</p>
-        <p>${repo.ref.target.history.totalCount} commits</p>
-        `;
-        $("ul.repos").append(`<li><div>${card}</div></li>`);
-      });
-    });
+
 });
--- django_server/project/django_server/settings-live.py
+++ django_server/project/django_server/settings-live.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,12 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.BibliographConfig',
-    'graphene_django'
 ]
-
-GRAPHENE = {
-    'SCHEMA': 'app.schema.schema'
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_server.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -85,7 +77,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -105,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -119,10 +109,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
