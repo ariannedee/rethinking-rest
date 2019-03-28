7a10deb0774c65648c26fa9ca5d585a7413ff85e
ariannedee <ariannedee@gmail.com>
2019-03-28 11:28:18 -0700

    add users

--- django_server/project/app/schema-live.py
+++ django_server/project/app/schema-live.py
@@ -1,9 +1,22 @@
 import graphene
+import graphene_django
+from django.contrib.auth.backends import UserModel
+
+class UserType(graphene_django.DjangoObjectType):
+    is_admin = graphene.Boolean()
+
+    def resolve_is_admin(self, info):
+        return self.is_staff
+
+    class Meta(object):
+        model = UserModel
+        only_fields = ('id', 'username')
+
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(description="Hello!")
+    users = graphene.List(UserType)
 
-    def resolve_hello(self, info):
-        return 'world'
+    def resolve_users(self, info):
+        return UserModel.objects.all()
 
 schema = graphene.Schema(query=Query)
