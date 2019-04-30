33bff907ba990c50468284ab536c989a9bf67fb5
ariannedee <ariannedee@gmail.com>
2019-04-30 11:17:43 -0700

    create hello world schema

--- django_server/project/django_server/urls-live.py
+++ django_server/project/django_server/urls-live.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
 ]
