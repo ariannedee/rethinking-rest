df931e6086299742e680c1ae7a3e75aef074122d
ariannedee <ariannedee@gmail.com>
2019-06-13 12:05:33 -0700

    Hello world

---
+++ node_server/project/src/schema-live.js
@@ -0,0 +1,18 @@
+const graphql = require('graphql');
+
+const queryType = new graphql.GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        hello: {
+            description: "Our first query",
+            type: graphql.GraphQLString,
+            resolve () {
+                return 'world';
+            }
+        }
+    }
+});
+
+const schema = new graphql.GraphQLSchema({query: queryType});
+
+module.exports = schema;
