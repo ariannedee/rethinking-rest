9ee10b8a36ffd80d3f6384c846663d8894923824
ariannedee <ariannedee@gmail.com>
2019-08-06 10:02:32 -0700

    Add card list

--- client/project/index-live.js
+++ client/project/index-live.js
@@ -15,7 +15,19 @@ fragment commitFragment on Repository {
 }
 `;
 
-let queryRepoList;
+const queryRepoList = `
+{
+	viewer {
+    name
+    repositories (first: 100) {
+      totalCount
+      nodes {
+        name
+      }
+    }
+  }
+}
+`;
 
 let mutationAddStar;
 
@@ -46,8 +58,16 @@ function starHandler(element) {
 
 $(window).ready(function() {
   // GET NAME AND REPOSITORIES FOR VIEWER
-  gqlRequest("{viewer{name}}", {}, (data) => {
+  gqlRequest(queryRepoList, {}, (data) => {
     const name = data.viewer.name;
     $('header h2').text("Hello " + name);
+    const repos = data.viewer.repositories;
+    if (repos.totalCount > 0) {
+      $("ul.repos").empty();
+      repos.nodes.forEach((repo) => {
+        const card = `<li><h3>${repo.name}</h3></li>`
+        $("ul.repos").append(card);
+      });
+    }
   });
 });
