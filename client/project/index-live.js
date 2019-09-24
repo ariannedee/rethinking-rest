0d67e414791c9c5b462e3d852e762667033e3548
ariannedee <ariannedee@gmail.com>
2019-09-24 11:12:16 -0700

    Hello world

--- client/project/index-live.js
+++ client/project/index-live.js
@@ -23,7 +23,24 @@ let mutationRemoveStar;
 
 function gqlRequest(query, variables, onSuccess) {
   // MAKE GRAPHQL REQUEST
-
+  $.post({
+    url: "https://api.github.com/graphql",
+    contentType: "application/json",
+    headers: {
+      Authorization: "bearer ..."
+    },
+    data: JSON.stringify({
+      query: query,
+      variables: variables
+    }),
+    success: (response) => {
+      if (response.errors) {
+        console.log(response.errors);
+      } else {
+        onSuccess(response.data);
+      }
+    }
+  });
 }
 
 function starHandler(element) {
@@ -33,5 +50,7 @@ function starHandler(element) {
 
 $(window).ready(function() {
   // GET NAME AND REPOSITORIES FOR VIEWER
-
+  gqlRequest("{viewer { name } }", {}, (data) => {
+    $('header h2').text(`Hello ${data.viewer.name}`);
+  });
 });
