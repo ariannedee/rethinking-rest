06cf8b34bb1741ab9eeb56d6ba4793a139803c32
ariannedee <ariannedee@gmail.com>
2019-11-15 12:51:28 -0800

    add avg rating

--- django_server/project/app/schema-live.py
+++ django_server/project/app/schema-live.py
@@ -1,6 +1,8 @@
 import graphene
 import graphene_django
 from django.contrib.auth.backends import UserModel
+from django.db.models import Avg
+
 from .models import Book, HasRead
 
 
@@ -16,6 +18,12 @@ class UserType(graphene_django.DjangoObjectType):
 
 
 class BookType(graphene_django.DjangoObjectType):
+    average_rating = graphene.Float()
+
+    def resolve_average_rating(self, info):
+        query = self.read_by.all().aggregate(Avg('rating'))
+        return query['rating__avg']
+
     class Meta(object):
         model = Book
 
