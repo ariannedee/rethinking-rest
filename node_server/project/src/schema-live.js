828afdd63fe408de9168d6eea2e6dbf206108124
ariannedee <ariannedee@gmail.com>
2019-11-15 12:17:53 -0800

    Add books

--- node_server/project/src/schema-live.js
+++ node_server/project/src/schema-live.js
@@ -20,6 +20,42 @@ const UserType = new graphql.GraphQLObjectType({
   }
 });
 
+const BookType = new graphql.GraphQLObjectType({
+  name: 'Book', 
+  fields: {
+    id: {
+      type: graphql.GraphQLID,
+      resolve(book) {
+        return book.id;
+      }
+    },
+    title: {
+      type: graphql.GraphQLString,
+      resolve(book) {
+        return book.title;
+      }
+    },
+    author: {
+      type: graphql.GraphQLString,
+      resolve(book) {
+        return book.author;
+      }
+    },
+    fiction: {
+      type: graphql.GraphQLBoolean,
+      resolve(book) {
+        return book.fiction;
+      }
+    },
+    publishedYear: {
+      type: graphql.GraphQLInt,
+      resolve(book) {
+        return book.publishedYear;
+      }
+    },
+  }
+});
+
 const queryType = new graphql.GraphQLObjectType({
   name: 'Query',
   fields: {
@@ -28,7 +64,13 @@ const queryType = new graphql.GraphQLObjectType({
       resolve(root) {
         return knex('user');
       }
-    }
+    },
+    books: {
+      type: new graphql.GraphQLList(BookType),
+      resolve(root) {
+        return knex('book');
+      }
+    },
   }
 });
 
