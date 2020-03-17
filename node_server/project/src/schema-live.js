ddd700612eb2442326f5e4eb93b20af02cc8688b
ariannedee <ariannedee@gmail.com>
2020-03-17 12:12:48 -0700

    hello world

---
+++ node_server/project/src/schema-live.js
@@ -0,0 +1,18 @@
+const graphql = require('graphql');
+
+const queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  description: 'All the things you can query',
+  fields: {
+    hello: {
+      description: "hello world",
+      type: graphql.GraphQLString,
+      resolve () {
+          return 'world';
+      }
+    }
+  }
+});
+
+const schema = new graphql.GraphQLSchema({query: queryType});
+module.exports = schema;
