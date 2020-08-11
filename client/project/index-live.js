d0c4e995e07b9249e4b8184dd14485c5b47c2b34
ariannedee <ariannedee@gmail.com>
2020-08-11 08:31:44 -0700

    Revert live files to starting state

--- client/project/index-live.js
+++ client/project/index-live.js
@@ -15,26 +15,7 @@ fragment commitFragment on Repository {
 }
 `;
 
-const queryRepoList = `
-{
-  viewer {
-    name
-		repositories (first: 12, orderBy: {field: STARGAZERS, direction: DESC}) {
-      totalCount
-      nodes {
-        name
-        viewerHasStarred
-        openIssues: issues(states: OPEN) {
-          totalCount
-        }
-        openPRs: pullRequests(states: OPEN) {
-          totalCount
-        }
-        ... commitFragment
-      }
-    }  
-  }
-}` + commitFragment;
+let queryRepoList;
 
 let mutationAddStar;
 
@@ -42,24 +23,7 @@ let mutationRemoveStar;
 
 function gqlRequest(query, variables, onSuccess) {
   // MAKE GRAPHQL REQUEST
-  $.post({
-    url: "https://api.github.com/graphql",
-    contentType: "application/json",
-    headers: {
-      Authorization: `bearer ${env.GITHUB_PERSONAL_ACCESS_TOKEN}`
-    },
-    data: JSON.stringify({
-      query: query,
-      variables: variables
-    }),
-    success: (response) => {
-      if (response.errors) {
-        console.log(response.errors);
-      } else {
-        onSuccess(response.data);
-      }
-    }
-  });
+
 }
 
 function starHandler(element) {
@@ -69,28 +33,5 @@ function starHandler(element) {
 
 $(window).ready(function() {
   // GET NAME AND REPOSITORIES FOR VIEWER
-  gqlRequest(queryRepoList, {}, (data) => {
-    $('header h2').text(`Hello ${data.viewer.name}`);
-    const repos = data.viewer.repositories;
-    console.log(repos);
-    if (repos.totalCount > 0) {
-      $('ul.repos').empty();
-      repos.nodes.forEach((repo) => {
-        let commits = 0;
-        if (repo.ref) {
-          commits = repo.ref.target.history.totalCount;
-        }
-        const star = repo.viewerHasStarred ? fullStar : emptyStar;
-        const card = `
-        <div>
-        <h3>${repo.name} <span class="star">${star}</span></h3>
-        <p>${repo.openIssues.totalCount} open issues</p>
-        <p>${repo.openPRs.totalCount} open pull requests</p>
-        <p>${commits} commits</p>
-        </div>
-        `;
-        $('ul.repos').append(`<li>${card}</li>`)
-      });
-    }
-  })
+
 });
