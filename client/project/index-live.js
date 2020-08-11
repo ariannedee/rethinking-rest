bc63a49e3c1c05ee37bc2fe39180ab9a74b74caa
ariannedee <ariannedee@gmail.com>
2020-08-11 10:50:18 -0700

    Make repo cards

--- client/project/index-live.js
+++ client/project/index-live.js
@@ -15,7 +15,18 @@ fragment commitFragment on Repository {
 }
 `;
 
-let queryRepoList;
+const queryRepoList = `
+{
+  viewer {
+    name
+    repositories (first: 12) {
+      totalCount
+      nodes {
+        name
+      }
+    }
+  }
+}`;
 
 let mutationAddStar;
 
@@ -45,7 +56,19 @@ function starHandler(element) {
 
 $(window).ready(function() {
   // GET NAME AND REPOSITORIES FOR VIEWER
-  gqlRequest("{viewer{name}}", {}, (data) => {
+  gqlRequest(queryRepoList, {}, (data) => {
+    console.log(data);
     $('header h2').text(`Hello ${data.viewer.name}`);
+    const repos = data.viewer.repositories;
+    if (repos.totalCount > 0){
+      $('ul.repos').empty();
+    }
+    repos.nodes.forEach((repo) => {
+      const li = `
+      <li>
+        <h3>${repo.name}</h3>
+      </li>`;
+      $('ul.repos').append(li);
+    });
   });
 });
