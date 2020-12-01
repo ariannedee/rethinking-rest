46a54dfa21ee811b1cca2c996438a94009bf35a8
ariannedee <ariannedee@gmail.com>
2020-12-01 11:42:43 -0800

    Hello world

--- node_server/project/src/schema-live.js
+++ node_server/project/src/schema-live.js
@@ -1,131 +1,19 @@
 const graphql = require('graphql');
-const knex = require('../db');
-
-const UserType = new graphql.GraphQLObjectType({
-    name: 'User',
-    fields: () => {
-        return {
-            id: {
-                type: graphql.GraphQLID,
-                resolve(user) {
-                    return user.id;
-                }
-            },
-            username: {
-                type: graphql.GraphQLString,
-                resolve(user) {
-                    return user.username;
-                }
-            },
-            isAdmin: {
-                type: graphql.GraphQLBoolean,
-                resolve(user) {
-                    return user.role === 'admin';
-                }
-            },
-            booksRead: {
-                type: graphql.GraphQLList(HasReadType),
-                resolve(user) {
-                    return knex('hasRead').where('userId', user.id);
-                }
-            },
-            avgRating: {
-                type: graphql.GraphQLFloat,
-                async resolve(user) {
-                    let query = await knex('hasRead')
-                    .where('userId', user.id)
-                    .avg('rating as avg_rating')
-                    .first();
-                    return query['avg_rating']
-                }
-            }
-        }
-    }
-});
-
-const BookType = new graphql.GraphQLObjectType({
-    name: 'Book',
-    fields: () => {
-        return {
-            id: {
-                type: graphql.GraphQLID,
-                resolve(book) {
-                    return book.id;
-                }
-            },
-            title: {
-                type: graphql.GraphQLString,
-                resolve(book) {
-                    return book.title;
-                }
-            },
-            author: {
-                type: graphql.GraphQLString,
-                resolve(book) {
-                    return book.author;
-                }
-            },
-            fiction: {
-                type: graphql.GraphQLBoolean,
-                resolve(book) {
-                    return book.fiction;
-                }
-            },
-            publishedYear: {
-                type: graphql.GraphQLInt,
-                resolve(book) {
-                    return book.publishedYear;
-                }
-            }
-        }
-    }
-});
-
-const HasReadType = new graphql.GraphQLObjectType({
-    name: 'HasRead',
-    fields: {
-        rating: {
-            type: graphql.GraphQLInt,
-            resolve(hasRead) {
-                return hasRead.rating;
-            }
-        },
-        book: {
-            type: BookType,
-            resolve(hasRead) {
-                return knex('book').where('id', hasRead.bookId).first();
-            }
-        }
-    }
-});
+const { __esModule } = require('graphql/graphql');
 
 const queryType = new graphql.GraphQLObjectType({
-    name: 'Query',
-    fields: {
-        users: {
-            type: graphql.GraphQLList(UserType),
-            resolve() {
-                return knex('user');
-            }
-        },
-        books: {
-            args: {
-                fiction: {
-                    type: graphql.GraphQLBoolean
-                }
-            },
-            type: graphql.GraphQLList(BookType),
-            resolve(root, args) {
-                let query = knex('book');
-                const fiction = args.fiction;
-                if (fiction != undefined) {
-                    query = query.where('fiction', fiction);
-                }
-                return query;
-            }
-        }
+  name: 'Query',
+  description: 'The root query',
+  fields: {
+    hello: {
+      type: graphql.GraphQLString,
+      resolve () {
+        return 'world';
+      }
     }
+  }
 });
 
 const schema = new graphql.GraphQLSchema({query: queryType});
+
 module.exports = schema;
