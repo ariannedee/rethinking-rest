2ca663575f1d1e44deee6f0e32382f6c25aea559
ariannedee <ariannedee@gmail.com>
2020-12-01 11:49:06 -0800

    Hello world

--- django_server/project/app/schema-live.py
+++ django_server/project/app/schema-live.py
@@ -1,44 +1,10 @@
 import graphene
-import graphene_django
-from django.contrib.auth.backends import UserModel
-from .models import Book, HasRead
-from django.db.models import Avg
-
-
-class UserType(graphene_django.DjangoObjectType):
-    is_admin = graphene.Boolean()
-    average_rating = graphene.Float()
-
-    def resolve_is_admin(self, info):
-        return self.is_staff
-
-    def resolve_average_rating(self, info):
-        query = self.books_read.all().aggregate(Avg('rating'))
-        return query['rating__avg']
-
-    class Meta:
-        model = UserModel
-        only_fields = ('id', 'username', 'books_read')
-
-
-class BookType(graphene_django.DjangoObjectType):
-    class Meta:
-        model = Book
-
-
-class HasReadType(graphene_django.DjangoObjectType):
-    class Meta:
-        model = HasRead
-
 
 class Query(graphene.ObjectType):
-    users = graphene.List(UserType)
-    books = graphene.List(BookType)
+    hello = graphene.String(description="hello world field")
 
-    def resolve_users(self, info):
-        return UserModel.objects.all()
+    def resolve_hello(self, info):
+        return 'world'
 
-    def resolve_books(self, info):
-        return Book.objects.all()
 
 schema = graphene.Schema(query=Query)
