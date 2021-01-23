67414f30a3f536fd57df1e5979366f4515f7546d
ariannedee <ariannedee@gmail.com>
2021-01-23 14:14:13 -0800

    Revert live files

--- client/project/index-live.js
+++ client/project/index-live.js
@@ -15,26 +15,7 @@ fragment commitFragment on Repository {
 }
 `;
 
-const queryRepoList = `
-{
-  viewer {
-    name
-    repositories (first: 12, orderBy: 
-      {field: CREATED_AT, direction: DESC}) {
-      totalCount
-      nodes {
-        name
-        openIssues: issues (states: OPEN){
-          totalCount
-        }
-        openPRs: pullRequests (states: OPEN){
-          totalCount
-        }
-        ... commitFragment
-      }
-    }
-  }
-}` + commitFragment;
+let queryRepoList;
 
 let mutationAddStar;
 
@@ -42,23 +23,7 @@ let mutationRemoveStar;
 
 function gqlRequest(query, variables, onSuccess) {
   // MAKE GRAPHQL REQUEST
-  $.post({
-    url: "https://api.github.com/graphql",
-    contentType: "application/json",
-    headers: {Authorization: `bearer ${env.GITHUB_PERSONAL_ACCESS_TOKEN}`},
-    data: JSON.stringify({
-      query: query,
-      variables: variables
-    }),
-    success: (response) => {
-      if (response.errors) {
-        console.log(response.errors);
-      } else {
-        console.log(response.data);
-        onSuccess(response.data);
-      }
-    }
-  });
+
 }
 
 function starHandler(element) {
@@ -68,26 +33,5 @@ function starHandler(element) {
 
 $(window).ready(function() {
   // GET NAME AND REPOSITORIES FOR VIEWER
-  gqlRequest(queryRepoList, {}, (data) => {
-    $("header h2").text(`Hello ${data.viewer.name}`);
-    const repos = data.viewer.repositories;
-    if (repos.totalCount > 0) {
-      $('ul.repos').empty();
-    }
-    repos.nodes.forEach((repo) => {
-      let commits = 0;
-      if (repo.ref) {
-        commits = repo.ref.target.history.totalCount;
-      }
-      const card = `
-      <li>
-      <h3>${repo.name}</h3>
-      <p>${repo.openIssues.totalCount} open issues</p>
-      <p>${repo.openPRs.totalCount} open PRs</p>
-      <p>${commits} commits</p>
-      </li>
-      `;
-      $('ul.repos').append(card);
-    });
-  });
+
 });
