0ff9bbd2fbd0519922359f65eaebba591e230148
ariannedee <ariannedee@gmail.com>
2021-02-04 11:57:49 -0800

    Add users

--- node_server/project/src/schema-live.js
+++ node_server/project/src/schema-live.js
@@ -1,12 +1,26 @@
 const graphql = require('graphql');
+const knex = require('../db');
+
+const UserType = new graphql.GraphQLObjectType({
+    name: 'User',
+    description: 'A user in the system',
+    fields: {
+        id: {
+            type: graphql.GraphQLID,
+            resolve(user) {
+                return user.id;
+            }
+        }
+    }
+})
 
 const queryType = new graphql.GraphQLObjectType({
     name: 'Query',
     fields: {
-        hello: {
-            type: graphql.GraphQLString,
+        users: {
+            type: new graphql.GraphQLList(UserType),
             resolve() {
-                return 'world';
+                return knex('user');
             }
         }
     }
