d985d872d54f1520352e787ff217ace60084f9ad
ariannedee <ariannedee@gmail.com>
2021-08-05 12:00:14 -0700

    Add users.id

--- node_server/project/src/schema-live.js
+++ node_server/project/src/schema-live.js
@@ -13,4 +13,34 @@ const queryType = new graphql.GraphQLObjectType({
 });
 
 const schema = new graphql.GraphQLSchema({query: queryType});
+module.exports = schema;const graphql = require('graphql');
+const knex = require('../db');
+
+const UserType = new graphql.GraphQLObjectType({
+  name: 'User',
+  description: 'A user',
+  fields: {
+    id: {
+      type: graphql.GraphQLID,
+      resolve(user) {
+        return user.id;
+      }
+    }
+  }
+});
+
+const queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    users: {
+      type: graphql.GraphQLList(UserType),
+      description: 'All users',
+      resolve() {
+        return knex('user');
+      }
+    }
+  }
+});
+
+const schema = new graphql.GraphQLSchema({query: queryType});
 module.exports = schema;
