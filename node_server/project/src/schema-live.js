df26800af2e2cbb7399c793eed12d17d9bbfd707
ariannedee <ariannedee@gmail.com>
2021-08-05 12:12:49 -0700

    Add books

--- node_server/project/src/schema-live.js
+++ node_server/project/src/schema-live.js
@@ -1,19 +1,4 @@
 const graphql = require('graphql');
-
-const queryType = new graphql.GraphQLObjectType({
-    name: 'Query',
-    fields: {
-        hello: {
-            type: graphql.GraphQLString,
-            resolve() {
-                return 'world';
-            }
-        }
-    }
-});
-
-const schema = new graphql.GraphQLSchema({query: queryType});
-module.exports = schema;const graphql = require('graphql');
 const knex = require('../db');
 
 const UserType = new graphql.GraphQLObjectType({
@@ -25,9 +10,59 @@ const UserType = new graphql.GraphQLObjectType({
       resolve(user) {
         return user.id;
       }
-    }
+    },
+    username: {
+      type: graphql.GraphQLString,
+      resolve(user) {
+        return user.username;
+      }
+    },
+    isAdmin: {
+      type: graphql.GraphQLBoolean,
+      resolve(user) {
+        return user.role == "admin";
+      }
+    },
   }
 });
+
+const BookType = new graphql.GraphQLObjectType({
+  name: 'Book',
+  description: 'A book',
+  fields: {
+    id: {
+      type: graphql.GraphQLID,
+      resolve(book) {
+        return book.id;
+      }
+    },
+    title: {
+      type: graphql.GraphQLString,
+      resolve(book) {
+        return book.title;
+      }
+    },
+    author: {
+      type: graphql.GraphQLString,
+      resolve(book) {
+        return book.author;
+      }
+    },
+    fiction: {
+      type: graphql.GraphQLBoolean,
+      resolve(book) {
+        return book.fiction;
+      }
+    },
+    publishedYear: {
+      type: graphql.GraphQLInt,
+      resolve(book) {
+        return book.publishedYear;
+      }
+    },
+  }
+});
+
 
 const queryType = new graphql.GraphQLObjectType({
   name: 'Query',
@@ -37,6 +72,12 @@ const queryType = new graphql.GraphQLObjectType({
       description: 'All users',
       resolve() {
         return knex('user');
+      }
+    },
+    books: {
+      type: graphql.GraphQLList(BookType),
+      resolve() {
+        return knex('book');
       }
     }
   }
