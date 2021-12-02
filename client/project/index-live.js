1da75119816545d13ebf1db9711d1cb510156997
ariannedee <ariannedee@gmail.com>
2021-12-02 11:53:31 -0800

    Say hello

--- client/project/index-live.js
+++ client/project/index-live.js
@@ -23,7 +23,27 @@ let mutationRemoveStar;
 
 function gqlRequest(query, variables, onSuccess) {
   // MAKE GRAPHQL REQUEST
-
+  $.post({
+    url: "https://api.github.com/graphql",
+    contentType: "application/json",
+    headers: {Authorization: `bearer ${env.GITHUB_PERSONAL_ACCESS_TOKEN}`},
+    data: JSON.stringify(
+      {
+        query: query,
+        variables: variables
+      }
+    ),
+    success: (response) => {
+      if (response.errors) {
+        console.log(response.errors);
+      } else {
+        onSuccess(response.data);
+      }
+    },
+    error: (error) => {
+      console.log(error);
+    }
+  });
 }
 
 function starHandler(element) {
@@ -33,5 +53,8 @@ function starHandler(element) {
 
 $(window).ready(function() {
   // GET NAME AND REPOSITORIES FOR VIEWER
-
+  gqlRequest("{viewer{name}}", {}, (data) => {
+    console.log(data);
+    $("header h2").text(`Hello ${data.viewer.name}`);
+  });
 });
