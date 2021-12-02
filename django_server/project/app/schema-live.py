6bc7fef1db7c140522231dea00deec274ca10577
ariannedee <ariannedee@gmail.com>
2021-12-02 13:44:29 -0800

    add has read

--- django_server/project/app/schema-live.py
+++ django_server/project/app/schema-live.py
@@ -2,7 +2,8 @@ import graphene
 import graphene_django
 
 from django.contrib.auth.backends import UserModel
-from .models import Book
+from .models import Book, HasRead
+
 
 class UserType(graphene_django.DjangoObjectType):
     is_admin = graphene.Boolean()
@@ -12,12 +13,17 @@ class UserType(graphene_django.DjangoObjectType):
 
     class Meta:
         model = UserModel
-        only_fields = ('id', 'username')
+        only_fields = ('id', 'username', 'books_read')
 
 
 class BookType(graphene_django.DjangoObjectType):
     class Meta:
         model = Book
+
+
+class HasReadType(graphene_django.DjangoObjectType):
+    class Meta:
+        model = HasRead
 
 
 class QueryType(graphene.ObjectType):
