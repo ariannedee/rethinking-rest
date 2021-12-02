ed9b2580be97fb504379059ec27581b8e0a0d032
ariannedee <ariannedee@gmail.com>
2021-12-02 13:31:00 -0800

    add books

--- node_server/project/src/schema-live.js
+++ node_server/project/src/schema-live.js
@@ -9,9 +9,57 @@ const UserType = new graphql.GraphQLObjectType({
             resolve(user) {
                 return user.id;
             }
-        }
+        },
+        username: {
+            type: graphql.GraphQLString,
+            resolve(user) {
+                return user.username;
+            }
+        },
+        isAdmin: {
+            type: graphql.GraphQLBoolean,
+            resolve(user) {
+                return user.role === "admin";
+            }
+        },
     }
-})
+});
+
+const BookType = new graphql.GraphQLObjectType({
+    name: 'Book',
+    fields: {
+        id: {
+            type: graphql.GraphQLID,
+            resolve(book) {
+                return book.id;
+            }
+        },
+        title: {
+            type: graphql.GraphQLString,
+            resolve(book) {
+                return book.title;
+            }
+        },
+        author: {
+            type: graphql.GraphQLString,
+            resolve(book) {
+                return book.author;
+            }
+        },
+        publishedYear: {
+            type: graphql.GraphQLInt,
+            resolve(book) {
+                return book.publishedYear;
+            }
+        },
+        fiction: {
+            type: graphql.GraphQLBoolean,
+            resolve(book) {
+                return book.fiction;
+            }
+        },
+    }
+});
 
 const queryType = new graphql.GraphQLObjectType({
     name: 'Query',
@@ -21,6 +69,12 @@ const queryType = new graphql.GraphQLObjectType({
             type: new graphql.GraphQLList(UserType),
             resolve() {
                 return knex('user');
+            }
+        },
+        books: {
+            type: new graphql.GraphQLList(BookType),
+            resolve() {
+                return knex('book');
             }
         }
     }
