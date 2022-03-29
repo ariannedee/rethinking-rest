102f016d49eaf665a328a46fafff56a465368600
ariannedee <ariannedee@gmail.com>
2022-03-29 08:57:40 -0700

    Revert live files

--- client/project/index-live.js
+++ client/project/index-live.js
@@ -3,16 +3,7 @@ const emptyStar = "☆";
 
 const commitFragment = `
 fragment commitFragment on Repository {
-  master: ref(qualifiedName: "master") {
-    target {
-      ... on Commit {
-        history {
-          totalCount
-        }
-      }
-    }
-  }
-  main: ref(qualifiedName: "main") {
+  ref(qualifiedName: "master") {
     target {
       ... on Commit {
         history {
@@ -24,85 +15,23 @@ fragment commitFragment on Repository {
 }
 `;
 
-const queryRepoList = `
-{
-  viewer {
-    name
-    repos: repositories(first: 30, orderBy: {field: CREATED_AT, direction: DESC}) {
-      totalCount
-      nodes {
-        name
-        openIssues: issues(states: OPEN) {
-          totalCount
-        }
-        openPRs: pullRequests(states: OPEN) {
-          totalCount
-        }
-        ... commitFragment
-      }
-    }
-  }
-}
-` + commitFragment;
+let queryRepoList;
 
 let mutationAddStar;
 
 let mutationRemoveStar;
 
 function gqlRequest(query, variables, onSuccess) {
-  // MAKE GRAPHQL REQUEST
-  $.post({
-    url: "https://api.github.com/graphql",
-    contentType: "application/json",
-    headers: {Authorization: `bearer ${env.GITHUB_PERSONAL_ACCESS_TOKEN}`},
-    data: JSON.stringify(
-      {
-        query: query,
-        variables: variables
-      }
-    ),
-    success: (response) => {
-      if (response.errors) {
-        console.log(response.errors);
-      } else {
-        onSuccess(response.data);
-      }
-    },
-    error: (error) => {
-      console.log(error);
-    }
-  });
+    // MAKE GRAPHQL REQUEST
+
 }
 
 function starHandler(element) {
-  // STAR OR UNSTAR REPO BASED ON ELEMENT STATE
+    // STAR OR UNSTAR REPO BASED ON ELEMENT STATE
 
 }
 
-$(window).ready(function() {
-  // GET NAME AND REPOSITORIES FOR VIEWER
-  gqlRequest(queryRepoList, {}, (data) => {
-    console.log(data);
-    $("header h2").text(`Hello ${data.viewer.name}`);
-    const repos = data.viewer.repos;
+$(window).ready(function () {
+    // GET NAME AND REPOSITORIES FOR VIEWER
 
-    if (repos.totalCount > 0) {
-      $("ul.repos").empty();
-    }
-    repos.nodes.forEach((repo) => {
-      let numCommits;
-      if (repo.main) {
-        numCommits = repo.main.target.history.totalCount;
-      } else {
-        numCommits = repo.master.target.history.totalCount;
-      }
-      const card = `<li>
-      <h3>${repo.name}</h3>
-      <p>${repo.openIssues.totalCount} open issues</p>
-      <p>${repo.openPRs.totalCount} open PRs</p>
-      <p>${numCommits} commits</p>
-      </li>`;
-      $("ul.repos").append(card);
-    });
-  });
 });
