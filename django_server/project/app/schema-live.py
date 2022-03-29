d5da57a8da9edf5c44c183e365b370d128b5bd66
ariannedee <ariannedee@gmail.com>
2022-03-29 12:06:27 -0700

    Hello world

---
+++ django_server/project/app/schema-live.py
@@ -0,0 +1,11 @@
+import graphene
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(description='test')
+
+    def resolve_hello(self, info):
+        return 'world'
+
+
+schema = graphene.Schema(query=Query)
