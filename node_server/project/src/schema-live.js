9b182f78bf974b3ffccbbe44ac5e130b271ae1cb
ariannedee <ariannedee@gmail.com>
2022-06-14 12:09:02 -0700

    get users

--- node_server/project/src/schema-live.js
+++ node_server/project/src/schema-live.js
@@ -1,12 +1,38 @@
 const graphql = require('graphql');
+const knex = require('../db');
+
+const UserType = new graphql.GraphQLObjectType({
+  name: 'User',
+  description: 'This is a user',
+  fields: {
+    id: {
+      type: graphql.GraphQLID,
+      resolve(user) {
+        return user.id;
+      }
+    },
+    username: {
+      type: graphql.GraphQLString,
+      resolve(user) {
+        return user.username;
+      }
+    },
+    isAdmin: {
+      type: graphql.GraphQLBoolean,
+      resolve(user) {
+        return user.role === 'admin';
+      }
+    }
+  }
+});
 
 const queryType = new graphql.GraphQLObjectType({
   name: 'Query',
   fields: {
-    hello: {
-      type: graphql.GraphQLString,
+    users: {
+      type: graphql.GraphQLList(UserType),
       resolve() {
-        return 'world';
+        return knex('user');
       }
     }
   }
