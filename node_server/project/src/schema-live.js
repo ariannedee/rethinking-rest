6f65e83fc25b2581325632dfc5e809735dea48f6
ariannedee <ariannedee@gmail.com>
2022-06-14 12:30:38 -0700

    add booksRead

--- node_server/project/src/schema-live.js
+++ node_server/project/src/schema-live.js
@@ -4,23 +4,31 @@ const knex = require('../db');
 const UserType = new graphql.GraphQLObjectType({
   name: 'User',
   description: 'This is a user',
-  fields: {
-    id: {
-      type: graphql.GraphQLID,
-      resolve(user) {
-        return user.id;
-      }
-    },
-    username: {
-      type: graphql.GraphQLString,
-      resolve(user) {
-        return user.username;
-      }
-    },
-    isAdmin: {
-      type: graphql.GraphQLBoolean,
-      resolve(user) {
-        return user.role === 'admin';
+  fields: () => {
+    return {
+      id: {
+        type: graphql.GraphQLID,
+        resolve(user) {
+          return user.id;
+        }
+      },
+      username: {
+        type: graphql.GraphQLString,
+        resolve(user) {
+          return user.username;
+        }
+      },
+      isAdmin: {
+        type: graphql.GraphQLBoolean,
+        resolve(user) {
+          return user.role === 'admin';
+        }
+      },
+      booksRead: {
+        type: graphql.GraphQLList(HasReadType),
+        resolve(user) {
+          return knex('hasRead').where('userId', user.id);
+        }
       }
     }
   }
@@ -61,6 +69,24 @@ const BookType = new graphql.GraphQLObjectType({
     }
   }
 });
+
+const HasReadType = new graphql.GraphQLObjectType({
+  name: 'HasRead',
+  fields: {
+    rating: {
+      type: graphql.GraphQLInt,
+      resolve(hasRead) {
+        return hasRead.rating;
+      }
+    },
+    book: {
+      type: BookType,
+      resolve(hasRead) {
+        return knex('book').where('id', hasRead.bookId).first();
+      }
+    }
+  }
+})
 
 const queryType = new graphql.GraphQLObjectType({
   name: 'Query',
