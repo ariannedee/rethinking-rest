8903691f210faab6510077e6d84771a0cda9599d
ariannedee <ariannedee@gmail.com>
2022-06-14 12:19:23 -0700

    add users and books

--- django_server/project/app/schema-live.py
+++ django_server/project/app/schema-live.py
@@ -1,10 +1,32 @@
 import graphene
+import graphene_django
+
+from django.contrib.auth.backends import UserModel
+from app.models import Book
+
+class UserType(graphene_django.DjangoObjectType):
+    is_admin = graphene.Boolean()
+
+    def resolve_is_admin(self, info):
+        return self.is_staff
+
+    class Meta:
+        model = UserModel
+        only_fields = ('id', 'username')
+
+class BookType(graphene_django.DjangoObjectType):
+    class Meta:
+        model = Book
 
 class Query(graphene.ObjectType):
-    hello = graphene.String()
+    users = graphene.List(UserType)
+    books = graphene.List(BookType, description="Books in the database")
 
-    def resolve_hello(self, info):
-        return 'world'
+    def resolve_users(self, info):
+        return UserModel.objects.all()
+
+    def resolve_books(self, info):
+        return Book.objects.all()
 
 
 schema = graphene.Schema(query=Query)
