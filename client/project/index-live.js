2c3c576781152dab7fa645c1f8636df9b5f32f69
ariannedee <ariannedee@gmail.com>
2022-09-20 22:10:19 -0700

    Reset live files

--- client/project/index-live.js
+++ client/project/index-live.js
@@ -24,54 +24,14 @@ fragment commitFragment on Repository {
 }
 `;
 
-const queryRepoList = `
-query ($username: String!) { 
-  user (login: $username) { 
-    name
-    repositories (
-      first: 12,
-      orderBy: {field: CREATED_AT, direction: DESC}
-    ) {
-      totalCount
-      nodes {
-        name
-        openIssues: issues (states: OPEN) {
-          totalCount
-        }
-        openPRs: pullRequests (states: OPEN) {
-          totalCount
-        }
-        ... commitFragment
-      }
-    }
-  }
-}` + commitFragment;
+let queryRepoList;
 
 let mutationAddStar;
 
 let mutationRemoveStar;
 
 function gqlRequest(query, variables, onSuccess) {
-  $.post({
-    url: 'https://api.github.com/graphql',
-    ContentType: 'application/json',
-    headers: {Authorization: `bearer ${env.GITHUB_PERSONAL_ACCESS_TOKEN}`},
-    data: JSON.stringify({
-      query: query,
-      variables: variables
-    }),
-    success: (response) => {
-      if (response.errors) {
-        console.log(response.errors);
-      } else {
-        onSuccess(response.data);
-        console.log(response.data);
-      }
-    },
-    error: (error) => {
-      console.log(error);
-    }
-  })
+  // MAKE GRAPHQL REQUEST
 }
 
 function starHandler(element) {
@@ -81,19 +41,5 @@ function starHandler(element) {
 
 $(window).ready(function() {
   // GET NAME AND REPOSITORIES FOR VIEWER
-  gqlRequest(queryRepoList, {username: 'ariannedee'}, (data) => {
-    $('h2').text(`Hello ${data.user.name}!`);
-    const repos = data.user.repositories;
-    if (repos.totalCount > 0) {
-      $('ul.repos').empty();
-    }
-    repos.nodes.forEach((repo) => {
-      const card = `<li>
-      <h3>${repo.name}</h3>
-      <p>${repo.openIssues.totalCount} open issues</p>
-      <p>${repo.openPRs.totalCount} open PRs</p>
-      </li>`;
-      $('ul.repos').append(card);
-    })
-  });
+
 });
